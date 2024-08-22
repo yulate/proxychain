080700f1d26166279e715b1ef5585f088f3915bf
yulate <yulate@qq.com>
2024-08-22 17:15:05 +0800

    修改macos下释放文件权限不足的问题

--- main.go
+++ main.go
@@ -25,26 +25,40 @@ func main() {
 func checkData() {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		fmt.Println("文件不存在, 释放中...")
-		err := os.Mkdir("data", 777)
+
+		// 删除已经存在的文件夹，以防它已经被创建但权限不正确
+		os.RemoveAll("data")
+
+		// 创建文件夹并指定正确的权限
+		err := os.Mkdir("data", 0755)
 		if err != nil {
 			fmt.Println("创建data文件夹失败：", err)
+			return
 		}
+
+		// 再次检查文件夹的权限，并修复不正确的权限
+		err = os.Chmod("data", 0755)
+		if err != nil {
+			fmt.Println("修正data文件夹权限失败：", err)
+			return
+		}
+
 		ReleaseTheFile()
 	}
 }
 
 //go:embed data/czutf8.dat
-var data embed.FS
+var embeddedData embed.FS
 
 func ReleaseTheFile() {
-	file, err := data.ReadFile("data/czutf8.dat")
+	file, err := embeddedData.ReadFile("data/czutf8.dat")
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.WriteFile(filePath, []byte(file), 0644)
+	err = os.WriteFile(filePath, file, 0644)
 	if err != nil {
-		fmt.Println("Failed to write file:", err)
+		fmt.Println("写入文件失败:", err)
 		return
 	}
 }
